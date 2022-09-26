e5447f955f16ecb6fb4464f27c29f4a068716763
Josh Ison <ison.84@buckeyemail.osu.edu>
2022-09-26 12:04:45 -0400

    Adding loop to grab user input

--- EntryWay.py
+++ EntryWay.py
@@ -1,12 +1,27 @@
 import redis
+import sqlparse
 
 def setup():
   return redis.Redis(host='localhost', port=6379, db=0)
+
+def query_redis(instance):
+  print("Input SQL query or STOP to quit")
+  raw = input()
+  while(raw != "STOP"):
+    # case statement or something that consumes sqlparse output
+    statements = sqlparse.split(raw)
+    print(statements)
+    print("Input SQL query or STOP to quit")
+    raw = input()
+
+  print("Quitting...")
+
 
 def main():
     redis_instance = setup()
     # test
     redis_instance.set('hello', 'redis')
+    query_redis(redis_instance)
 
 if __name__ == "__main__":
     main()
