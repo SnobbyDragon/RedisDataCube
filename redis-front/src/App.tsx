a37a5ceb5be1b50084016b182b2163fc61f4683e
Josh Ison <ison.84@buckeyemail.osu.edu>
2022-10-27 23:37:21 -0400

    Styling

--- redis-front/src/App.tsx
+++ redis-front/src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 import Table from "./Components/Table";
+import RedisP from "./assets/redisearch.png";
 
 function App() {
   const [query, setQuery] = useState("");
+  const [searchEnabled, setSearchEnabled] = useState(false);
   const [apiData, setApiData] = useState<Array<any>>([]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -27,26 +29,30 @@ function App() {
     <div className="App">
       <h1>Redis Cuber</h1>
       <div id="search-container">
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input
-            id="query-input"
-            type={"search"}
-            placeholder="SELECT ..."
-            name="query"
-            onChange={(box) => setQuery(box.target.value)}
-          ></input>
-        </form>
+        {!searchEnabled ? (
+          <div id="glass">
+            <img src={RedisP} onClick={() => setSearchEnabled(true)}></img>
+          </div>
+        ) : (
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+              id="query-input"
+              type={"search"}
+              placeholder="SELECT ..."
+              name="query"
+              onChange={(box) => setQuery(box.target.value)}
+            ></input>
+          </form>
+        )}
       </div>
       <div className="card">
         {apiData.length > 0 && <Table data={apiData} />}
-        {query.length === 0 ? (
-          <div>
-            <img src="../loading-gif.gif"></img>
-          </div>
+        {searchEnabled ? <div>Query: {query}</div> : <></>}
+        {searchEnabled && apiData.length > 0 ? (
+          <a href="#top">Go to top</a>
         ) : (
-          <div>Query: {query}</div>
+          <></>
         )}
-        <a href="#top">Go to top</a>
       </div>
     </div>
   );
